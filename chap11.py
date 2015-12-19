d85f7f0ab1ccf3e95809832defeb9a46244aae87
basterik <basterik@users.noreply.github.com>
2015-12-19 13:42:32 +0300

    Update chap11.py

--- chap11.py
+++ chap11.py
@@ -1,3 +1,5 @@
+#In this assignment you will read through and parse a file with text and numbers.
+#You will extract all the numbers in the file and compute the sum of the numbers.
 import re
 name = input("Enter file:")
 handle = open(name)
